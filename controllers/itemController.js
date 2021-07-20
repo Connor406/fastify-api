414d27b8bc3e61ad9e366b6c6b6436759e05adb3
Connor Mahan <mahan406@gmail.com>
2021-07-20 13:39:23 -0500

    fastify basic crud api example

---
+++ controllers/itemController.js
@@ -0,0 +1,50 @@
+let items = require("../Items");
+const { v4: uuid } = require("uuid");
+
+const getItems = (req, reply) => {
+  reply.send(items);
+};
+
+const getItem = (req, reply) => {
+  const { id } = req.params; // destructure pull out ID from Items
+  const item = items.find((item) => item.id === id);
+  reply.send(item);
+};
+
+const addItem = (req, reply) => {
+  const { name } = req.body;
+  const item = {
+    id: uuid(),
+    name,
+  };
+  items = [...items, item];
+
+  reply.code(201).send(item);
+};
+
+const deleteItem = (req, reply) => {
+  const { id } = req.params;
+
+  items = items.filter((item) => item.id !== id);
+
+  reply.send({ message: `Item ${id} has been deleted` });
+};
+
+const updateItem = (req, reply) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  items = items.map((item) => (item.id === id ? { id, name } : item));
+
+  item = items.find((item) => item.id === id);
+
+  reply.send(item);
+};
+
+module.exports = {
+  getItem,
+  getItems,
+  addItem,
+  deleteItem,
+  updateItem,
+};
---
+++ server.js
@@ -0,0 +1,26 @@
+const fastify = require("fastify")({ logger: true });
+
+// Register Swagger => see docs for all options
+// Must be above Routes register
+fastify.register(require("fastify-swagger"), {
+  exposeRoute: true,
+  routePrefix: "/docs",
+  swagger: {
+    info: { title: "fastify-api" },
+  },
+});
+
+fastify.register(require("./routes/items")); // Register routes
+
+const PORT = 5000;
+
+const start = async () => {
+  try {
+    await fastify.listen(PORT);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
